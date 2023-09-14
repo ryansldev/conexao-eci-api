77c2db3e2a91a3e64eef627979d744bf0e0d14d5
Félix Vicente <felixvicent1306@gmail.com>
2023-09-14 16:52:59 -0300

    Change create student route to unauthenticate

--- src/routes.ts
+++ src/routes.ts
@@ -1,46 +1,45 @@
-import { FastifyInstance } from 'fastify'
+import { FastifyInstance } from "fastify";
 
-import { authMiddleware } from './middlewares/auth';
+import { StudentController } from "./controllers/Student";
+import { AuthController } from "./controllers/Auth";
+import { ChallengeController } from "./controllers/Challenge";
+import { ChallengedController } from "./controllers/Challenged";
+import { TeacherController } from "./controllers/Teacher";
 
-import { StudentController } from './controllers/Student'
-import { AuthController } from './controllers/Auth'
-import { ChallengeController } from './controllers/Challenge'
-import { ChallengedController } from './controllers/Challenged'
-import { TeacherController } from './controllers/Teacher';
+const authController = new AuthController();
 
-const authController = new AuthController()
+const studentController = new StudentController();
+const teacherController = new TeacherController();
 
-const studentController = new StudentController()
-const teacherController = new TeacherController()
-
-const challengeController = new ChallengeController()
-const challengedController = new ChallengedController()
+const challengeController = new ChallengeController();
+const challengedController = new ChallengedController();
 
 export async function appRoutes(app: FastifyInstance) {
-  app.addHook('onRequest', async (request, reply) => {
+  app.addHook("onRequest", async (request, reply) => {
     try {
-      await request.jwtVerify()
+      await request.jwtVerify();
     } catch (err) {
-      reply.send(err)
+      reply.send(err);
     }
-  })
-  
-  app.post('/students', studentController.create)
-  app.get('/students', studentController.list)
-  app.get('/students/:id', studentController.find)
+  });
 
-  app.post('/challenges', challengeController.create)
+  app.get("/students", studentController.list);
+  app.get("/students/:id", studentController.find);
 
-  app.post('/challenges/:id', challengedController.create)
+  app.post("/challenges", challengeController.create);
+
+  app.post("/challenges/:id", challengedController.create);
 }
 
 export async function appUnauthenticatedRoutes(app: FastifyInstance) {
-  app.post('/auth', authController.create)
-  app.get('/teachers', teacherController.list)
-  app.post('/teachers', teacherController.create)
+  app.post("/auth", authController.create);
+  app.get("/teachers", teacherController.list);
+  app.post("/teachers", teacherController.create);
 
-  app.get('/challenges', challengeController.list)
-  app.get('/challenges/:id', challengedController.list)
-  app.get('/challenges/:id/duel', challengedController.getDuel)
-  app.patch('/challenges/:id/duel', challengedController.duel)
+  app.post("/students", studentController.create);
+
+  app.get("/challenges", challengeController.list);
+  app.get("/challenges/:id", challengedController.list);
+  app.get("/challenges/:id/duel", challengedController.getDuel);
+  app.patch("/challenges/:id/duel", challengedController.duel);
 }
